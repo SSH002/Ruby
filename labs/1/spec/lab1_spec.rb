82ec1ff06a68b320503cc3ac731c7649135a0179
tpl <you@example.com>
2020-10-04 14:23:59 +0200

    Fix spec's

--- labs/1/spec/lab1_spec.rb
+++ labs/1/spec/lab1_spec.rb
@@ -1,51 +1,51 @@
 require './lab1.rb'
 
 RSpec.describe Lab1 do
-  describe '#Lab1' do
-    it 'test_C_to_C' do
-      expect(Lab1.new('30', 'C').converse('C')).to eq 30
+  describe '.lab1' do
+    context 'test_C_to_C' do
+      it { expect(Lab1.new('30', 'C').converse('C')).to eq 30 }
     end
-    it 'test_C_to_F' do
-      expect(Lab1.new('30', 'C').converse('K')).to eq 303.15
+    context 'test_C_to_F' do
+      it { expect(Lab1.new('30', 'C').converse('K')).to eq 303.15 }
     end
-    it 'test_C_to_K' do
-      expect(Lab1.new('30', 'C').converse('F')).to eq 86
+    context 'test_C_to_K' do
+      it { expect(Lab1.new('30', 'C').converse('F')).to eq 86 }
     end
-    it 'test_K_to_C' do
-      expect(Lab1.new('400', 'K').converse('C')).to eq 126.85
+    context 'test_K_to_C' do
+      it { expect(Lab1.new('400', 'K').converse('C')).to eq 126.85 }
     end
-    it 'test_K_to_K' do
-      expect(Lab1.new('400', 'K').converse('K')).to eq 400
+    context 'test_K_to_K' do
+      it { expect(Lab1.new('400', 'K').converse('K')).to eq 400 }
     end
-    it 'test_K_to_F' do
-      expect(Lab1.new('400', 'K').converse('F')).to eq 260.33
+    context 'test_K_to_F' do
+      it { expect(Lab1.new('400', 'K').converse('F')).to eq 260.33 }
     end
-    it 'test_F_to_C' do
-      expect(Lab1.new('86', 'F').converse('C')).to eq 30
+    context 'test_F_to_C' do
+      it { expect(Lab1.new('86', 'F').converse('C')).to eq 30 }
     end
-    it 'test_F_to_K' do
-      expect(Lab1.new('86', 'F').converse('K')).to eq 303.15
+    context 'test_F_to_K' do
+      it { expect(Lab1.new('86', 'F').converse('K')).to eq 303.15 }
     end
-    it 'test_F_to_F' do
-      expect(Lab1.new('86', 'F').converse('F')).to eq 86
+    context 'test_F_to_F' do
+      it { expect(Lab1.new('86', 'F').converse('F')).to eq 86 }
     end
-    it 'test_correct_number' do
-      expect(Lab1.new('86', 'F').number?('30.690')).to eq true
+    context 'test_correct_number' do
+      it { expect(Lab1.new('86', 'F').number?('30.690')).to eq true }
     end
-    it 'test_incorrect_number' do
-      expect(Lab1.new('86', 'F').number?('-309,O2')).to eq false
+    context 'test_incorrect_number' do
+      it { expect(Lab1.new('86', 'F').number?('-309,O2')).to eq false }
     end
-    it 'test_correct_scale' do
-      expect(Lab1.new('86', 'F').check_scale('f')).to eq true
+    context 'test_correct_scale' do
+      it { expect(Lab1.new('86', 'F').check_scale('f')).to eq true }
     end
-    it 'test_incorrect_scale' do
-      expect(Lab1.new('86', 'F').check_scale('V')).to eq false
+    context 'test_incorrect_scale' do
+      it { expect(Lab1.new('86', 'F').check_scale('V')).to eq false }
     end
-    it 'test_correct_scale' do
-      expect(Lab1.new('300', 'K').check_kelvin(300, 'K')).to eq true
+    context 'test_correct_scale' do
+      it { expect(Lab1.new('300', 'K').check_kelvin(300, 'K')).to eq true }
     end
-    it 'test_incorrect_scale' do
-      expect(Lab1.new('86', 'F').check_kelvin(-300, 'k')).to eq false
+    context 'test_incorrect_scale' do
+      it { expect(Lab1.new('86', 'F').check_kelvin(-300, 'k')).to eq false }
     end
   end
 end
--- labs/2/spec/lab2_spec.rb
+++ labs/2/spec/lab2_spec.rb
@@ -2,18 +2,18 @@ require './csv_reader.rb'
 require './lab2.rb'
 
 RSpec.describe Lab2 do
-  describe '#Lab2' do
-    it 'test_maximum' do
-      expect(Lab2.new.get_maximum).to eq 35_566
+  describe '.lab2' do
+    context 'test_maximum' do
+      it { expect(Lab2.new.get_maximum).to eq 35_566 }
     end
-    it 'test_minimum' do
-      expect(Lab2.new.get_minimum).to eq 3
+    context 'test_minimum' do
+      it { expect(Lab2.new.get_minimum).to eq 3 }
     end
-    it 'test_average' do
-      expect(Lab2.new.get_average).to eq 517.55
+    context 'test_average' do
+      it { expect(Lab2.new.get_average).to eq 517.55 }
     end
-    it 'test_bsv' do
-      expect(Lab2.new.get_bsv).to eq 3_922_559.76
+    context 'test_bsv' do
+      it { expect(Lab2.new.get_bsv).to eq 3_922_559.76 }
     end
   end
 end
