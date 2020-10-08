03b328f3bf885d3f9607555d356b10c30b3b301e
tpl <you@example.com>
2020-10-08 08:35:32 +0200

    Fix Lab2 calculations using inject

--- labs/2/csv_reader.rb
+++ labs/2/csv_reader.rb
@@ -1,12 +1,18 @@
 require 'csv'
 
 class Reader
-  attr_reader :data, :rows, :columns
+  attr_reader :data, :rows, :columns, :array
 
   def initialize
     @data = CSV.read('data-20161226-structure-20161225.csv')
 
     @rows = data.length
     @columns = data[0].length
+
+    @array = []
+
+    (1...rows).each do |i|
+      @array[i - 1] = data[i][0].to_f
+    end
   end
 end
--- labs/2/statistic.rb
+++ labs/2/statistic.rb
@@ -8,47 +8,23 @@ class Statistic
   end
 
   def search_maximum
-    @maximum = 0
-
-    (1...@reader.rows).each do |i|
-      @maximum = @reader.data[i][0].to_f if @reader.data[i][0].to_f > @maximum
-    end
-
-    @maximum
+    @maximum = @reader.array.max
   end
 
   def search_minimum
-    @minimum = 1_000_000
-
-    (1...@reader.rows).each do |i|
-      @minimum = @reader.data[i][0].to_f if @reader.data[i][0].to_f < @minimum
-    end
-
-    @minimum
+    @minimum = @reader.array.min
   end
 
   def search_average
-    @average = 0
-    count = 0
-
-    (1...@reader.rows).each do |i|
-      @average += @reader.data[i][0].to_f
-      count += 1
-    end
-
-    @average = (@average / count).round(2)
+    avr = @reader.array.inject { |s, n| s + n } / @reader.array.count
+    @average = avr.round(2)
   end
 
   def search_bsv
     search_average
-    count = -1
-    sum = 0
 
-    (1...@reader.rows).each do |i|
-      sum += (@reader.data[i][0].to_f - @average)**2
-      count += 1
-    end
+    sum = @reader.array.inject(0) { |s, n| s + (n - @average)**2 }
 
-    @bsv = (sum / count).round(2)
+    @bsv = (sum / (@reader.array.count - 1)).round(2)
   end
 end
